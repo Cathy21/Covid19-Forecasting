1db5cf0ce2b5a65d6d0f48492c512d6663f94d2a
cst35 <cathystao@gmail.com>
2021-04-26 14:00:36 -0400

    a

--- SEIRTest.m
+++ SEIRTest.m
@@ -1,18 +1,18 @@
-A = 28112;
-beta = .5;
+A = 50;
+beta = 0.5;
 c = .12;
-d = .0058;
-delta = .0685;
-alpha = .6;%1/5.2;
-eta = 1/14;
+d = 0.2;
+delta = .25;
+alpha = .3;%1/5.2;
+eta = 0.25;
 rho1 = .64;
 rho2 = .78;
-b1 = .07122;
-b2 = .11013;
-sigma = 0;%.119732;
-p = .96657;
+b1 = .25;
+b2 = .8;
+sigma = 0.2;%.119732;
+p = .78;
 %M = .916499;
-M=0;
+M=0.8;
 
 rhs = @(t, xt) [A - beta*(1-rho1)*(1-rho2)*xt(1)*xt(2) + b1*xt(3) - d*xt(1) - p*xt(1)*M;
     beta*(1-rho1)*(1-rho2)*xt(1)*xt(2) - b2*xt(2) - alpha*xt(2) - sigma*xt(2) - d*xt(2);
@@ -20,7 +20,7 @@ rhs = @(t, xt) [A - beta*(1-rho1)*(1-rho2)*xt(1)*xt(2) + b1*xt(3) - d*xt(1) - p*
     alpha*xt(2) + c*xt(3) - (eta + d + delta)*xt(4);
     eta*xt(4) + sigma*xt(2) - d*xt(5) + p*xt(1)*M];
 
-x0 = [1374333, 312, 945, 0, 0];
+x0 = [50, 10, 5, 1, 0];
 [time, x] = ode45(rhs,[0,50],x0);
 
 figure(1)
